28862b19de39b7abda8b8625def30bc19878a3af
Boyan Li <boyanl@nvidia.com>
2026-08-18 11:53:45 -0700

    skip task scheduling test
    Signed-off-by: Boyan Li <boyanl@nvidia.com>

--- experimental/task_scheduling/test/test_import.py
+++ experimental/task_scheduling/test/test_import.py
@@ -2,6 +2,11 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import pytest
+
+pytest.importorskip("cuda.lang", reason="Skipping task_scheduling test: module not found")
+pytest.importorskip("task_scheduling", reason="Skipping task_scheduling test: module not found")
+
 
 def test_imports():
     import cuda.lang
